91d7626234e9eddec43612f8b55617d8f4c443f9
Leoyalta <149539157+Leoyalta@users.noreply.github.com>
2025-07-02 13:40:01 +0200

    fix-gitignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   mapboxToken:
-    'sk.eyJ1IjoibGVveWFsdGEiLCJhIjoiY21jbHBhMHo2MGJyNTJqczV4ZHQwMjB4cyJ9.yJUS4IpH3tJzGihXgDFeBw',
+    'sk.eyJ1IjoibGVveWFsdGEiLCJhIjoiY21jbHY1dXY1MGZ0ZDJrczdmYWJ3NWRvZyJ9.ksJs35biWfm9wwdaWDn2cg',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   mapboxToken:
-    'pk.eyJ1IjoibGVveWFsdGEiLCJhIjoiY21jbHA2ZGlqMDFnMDJocW54ZTE5amI4aSJ9.rI5V7ywwnYgAQnJXwKekZQ',
+    'pk.eyJ1IjoibGVveWFsdGEiLCJhIjoiY21jbHYwOWt1MGQ0eDJuc2J2M3p3bnFrdCJ9.pStOKyeaKhHCmNkp-vieVQ',
 };
