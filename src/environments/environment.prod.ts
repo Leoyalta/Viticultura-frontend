dfc37d10893e50e7bdbabb51124f48e0451fcbcd
Leoyalta <149539157+Leoyalta@users.noreply.github.com>
2025-07-02 20:20:57 +0200

    Remove environments and ignore them

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  mapboxToken:
-    'sk.eyJ1IjoibGVveWFsdGEiLCJhIjoiY21jbHY1dXY1MGZ0ZDJrczdmYWJ3NWRvZyJ9.ksJs35biWfm9wwdaWDn2cg',
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  mapboxToken:
-    'pk.eyJ1IjoibGVveWFsdGEiLCJhIjoiY21jbHYwOWt1MGQ0eDJuc2J2M3p3bnFrdCJ9.pStOKyeaKhHCmNkp-vieVQ',
-};
