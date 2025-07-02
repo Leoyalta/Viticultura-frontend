3c0670cbe15579b280b15d6d7014157d62a358d3
Leoyalta <149539157+Leoyalta@users.noreply.github.com>
2025-07-02 13:51:23 +0200

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  mapboxToken:
-    'sk.eyJ1IjoibGVveWFsdGEiLCJhIjoiY21jbHBhMHo2MGJyNTJqczV4ZHQwMjB4cyJ9.yJUS4IpH3tJzGihXgDFeBw',
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  mapboxToken:
-    'pk.eyJ1IjoibGVveWFsdGEiLCJhIjoiY21jbHA2ZGlqMDFnMDJocW54ZTE5amI4aSJ9.rI5V7ywwnYgAQnJXwKekZQ',
-};
