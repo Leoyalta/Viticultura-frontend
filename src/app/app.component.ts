0ebcc967ded6e6a3b54126d206179098e4042c3b
Leoyalta <149539157+Leoyalta@users.noreply.github.com>
2025-07-03 14:20:12 +0200

    add-.env

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavListComponent } from './core/nav-list/nav-list.component';
+import { NavListComponent } from './core/layout/nav-list/nav-list.component';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ThemeService } from './core/themes/services/themes/theme.service';
-import { TabNavigationComponent } from './core/tab-navigation/tab-navigation.component';
+import { TabNavigationComponent } from './core/layout/tab-navigation/tab-navigation.component';
 
 @Component({
   selector: 'app-root',
