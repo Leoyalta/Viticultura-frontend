bfbd1bb3af3148f503afc70fb3dfbc9deb7b620e
Leoyalta <149539157+Leoyalta@users.noreply.github.com>
2025-07-09 23:52:48 +0200

    add-create-location-map

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,19 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { LoginComponent } from './core/auth/components/login/login.component';
 import { RegisterComponent } from './core/auth/components/register/register.component';
+
 import { ProductComponent } from './features/products/components/product/product.component';
 import { NotFoundPageComponent } from './features/not-found-page/not-found-page.component';
 import { ProductsListComponent } from './features/products/components/products-list/products-list.component';
 import { AddNewProductComponent } from './features/products/components/add-new-product/add-new-product.component';
 import { UpdateProductComponent } from './features/products/components/update-product/update-product.component';
+
 import { ClientListComponent } from './features/clients/components/client-list/client-list.component';
 import { ClientProfileComponent } from './features/clients/components/client-profile/client-profile.component';
 import { AddNewClientComponent } from './features/clients/components/add-new-client/add-new-client.component';
 import { UpdateClientProfileComponent } from './features/clients/components/update-client-profile/update-client-profile.component';
+
+import { CreateClientLocationComponent } from './features/locations/components/create-client-location/create-client-location.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -28,6 +32,11 @@ export const routes: Routes = [
   { path: 'clients/:id', component: ClientProfileComponent },
   { path: 'addNewClient', component: AddNewClientComponent },
   { path: 'updateClient/:id', component: UpdateClientProfileComponent },
+
+  {
+    path: 'clients/:id/locations/new',
+    component: CreateClientLocationComponent,
+  },
 
   { path: '**', component: NotFoundPageComponent },
 ];
