1c8b8240d988629ff01f0a868c1b05d89e3c0100
Leoyalta <149539157+Leoyalta@users.noreply.github.com>
2025-07-16 22:57:51 +0200

    Save current working changes before history rewrite

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,1 @@
-export const environment = {
-  production: true,
-  firebase: {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
-    measurementId: '',
-  },
-  MAPBOX_TOKEN: '',
-};
+export const environment = {};
